072d88450934dd816409fc9e3c864c83d5af6314
akash <aksbju@gmail.com>
2023-06-19 18:48:56 +0530

    Update

--- lib/buttons/primary.button.dart
+++ lib/buttons/primary.button.dart
@@ -2,20 +2,29 @@ import 'package:flutter/material.dart';
 
 class PrimaryButton extends StatelessWidget {
   const PrimaryButton(
-      {Key? key, required this.color, required this.child, this.onPressed})
+      {Key? key,
+      required this.color,
+      required this.child,
+      this.onPressed,
+      this.width,
+      this.height})
       : super(key: key);
 
   final Color color;
   final Widget child;
+  final double? width;
+  final double? height;
   final Function()? onPressed;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 50.0,
+      width: width,
+      height: height,
       child: ElevatedButton(
         onPressed: onPressed,
         style: ElevatedButton.styleFrom(
           backgroundColor: color,
+          foregroundColor: Colors.white,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10.0),
           ),
